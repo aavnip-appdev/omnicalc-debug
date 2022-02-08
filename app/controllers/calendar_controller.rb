b08afc1a7454414a6860b1aa15ee03c10283fee5
aavnip <aavnip@chicagobooth.edu>
2022-02-08 15:16:14 +0000

    OmniCalc Debug Calendar Fixed

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,19 +1,18 @@
 class CalendarController < ApplicationController
   
   def invite
-    
     render({ :template => "calendar_templates/new.html.erb"})
   end
 
   def generate
-    @name = params.fetch("event_name")
-    @details = params.fetch("detail")
-    @location = params.fetch("event_location")
+    @name = params.fetch("name")
+    @details = params.fetch("details")
+    @location = params.fetch("location")
 
-    regular_start_date = params.fetch("start_time")
+    regular_start_date = params.fetch("start_date")
     @start_date = regular_start_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
 
-    regular_end_date = params.fetch("end_time")
+    regular_end_date = params.fetch("end_date")
     @end_date = regular_end_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
     
     @invite = "https://www.google.com/calendar/render?action=TEMPLATE&text=#{@name}&details=#{@details}&location=#{@location}&dates=#{@start_date}/#{@end_date}"
